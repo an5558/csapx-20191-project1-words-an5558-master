84afbbd7f12aa407c3f03a4c9a0326f3a7197783
Ceiran <33044342+Ceiran@users.noreply.github.com>
2019-09-20 17:52:25 -0400

    Adds command line arguments for word_count.py

--- src/word_count.py
+++ src/word_count.py
@@ -1,6 +1,19 @@
+"""
+CSAPX Project 1: Zipf's Law
+
+Counts the number of occurences of a word in a year, given the word and file.
+
+Author: Ayane Naito
+"""
+
+import argparse
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("word", help="a word to display the total occurrences of")
+    parser.add_argument("filename", help="a comma separated value unigram file")
+    args = parser.parse_args()
+    print(args.word)
 
 if __name__ == '__main__':
     main()
