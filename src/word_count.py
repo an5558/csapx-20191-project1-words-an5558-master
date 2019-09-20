079fabba180a14a90c590cf2b086e85085c3bc06
Ceiran <33044342+Ceiran@users.noreply.github.com>
2019-09-20 17:59:13 -0400

    Adds error message if input file not found

--- src/word_count.py
+++ src/word_count.py
@@ -7,6 +7,8 @@ Author: Ayane Naito
 """
 
 import argparse
+import sys
+import os
 
 def main():
     parser = argparse.ArgumentParser()
@@ -14,6 +16,10 @@ def main():
     parser.add_argument("filename", help="a comma separated value unigram file")
     args = parser.parse_args()
     print(args.word)
+    if os.path.isfile(args.filename):
+        pass
+    else:
+       sys.stderr.write("Error: " + str(args.filename) + " does not exist!")
 
 if __name__ == '__main__':
     main()
