6aab51fd3c29d0fa4b9774c47aa23cd2896dd20d
Ceiran <33044342+Ceiran@users.noreply.github.com>
2019-09-20 18:41:55 -0400

    Creates read_words and completes word count activity

--- src/word_count.py
+++ src/word_count.py
@@ -9,21 +9,21 @@ Author: Ayane Naito
 import argparse
 import sys
 import os
+from src import words_util
 
 def main():
-    words = {}
     parser = argparse.ArgumentParser()
     parser.add_argument("word", help="a word to display the total occurrences of")
     parser.add_argument("filename", help="a comma separated value unigram file")
     args = parser.parse_args()
-    print(args.word)
     if os.path.isfile(args.filename):
+        words = words_util.read_words(args.filename)
         if args.word in words:
-            pass
+            print(args.word + ": " + str(words[args.word]))
         else:
             sys.stderr.write("Error: " + str(args.word) + " does not appear!")
     else:
-       sys.stderr.write("Error: " + str(args.filename) + " does not exist!")
+        sys.stderr.write("Error: " + str(args.filename) + " does not exist!")
 
 if __name__ == '__main__':
     main()
--- src/words_util.py
+++ src/words_util.py
@@ -5,3 +5,15 @@ Contains functions used across the project to aid in reading files and managing 
 
 Author: Ayane Naito
 """
+
+import csv
+
+def read_words(file):
+    reader = csv.reader(open(file))
+    dict = {}
+    for row in reader:
+        if row[0] in dict:
+            dict[row[0]] = int(dict[row[0]]) + int(row[2])
+        else:
+            dict[row[0]] = int(row[2])
+    return dict
