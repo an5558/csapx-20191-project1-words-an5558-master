3007fc8ee158a9f9fd0c38c0a354545572657c84
Ceiran <33044342+Ceiran@users.noreply.github.com>
2019-10-04 13:31:42 -0400

    Add positional and optional arguments for word_freq

--- src/word_freq.py
+++ src/word_freq.py
@@ -1,5 +1,24 @@
+"""
+CSAPX Project 1: Zipf's Law
+
+
+
+Author: Ayane Naito
+"""
+
+
+import argparse
+import sys
+import os
+from src import words_util
+
 def main():
-    pass
+    parser = argparse.ArgumentParser
+    parser.add_argument("word", help="a word to display the overall ranking of")
+    parser.add_argument("filename", help="a comma separated value unigram file")
+    parser.add_argument("-o", "--output", help="display the top OUTPUT (#) ranked words by number of occurrences", action="store_true")
+    parser.add_argument("-p", "--plot", help="plot the word rankings from top to bottom based on occurrences", action="store_true")
+    args = parser.parse.args()
 
 if __name__ == '__main__':
     main()
