aa5eaeefffd1c19468099bdb39e0f37800253e87
Ceiran <33044342+Ceiran@users.noreply.github.com>
2019-10-06 01:16:26 -0400

    Adds error handling for missing file

--- src/word_length.py
+++ src/word_length.py
@@ -11,6 +11,7 @@ import sys
 import os
 from src import words_util
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("start", help="the starting year range")
@@ -18,6 +19,11 @@ def main():
     parser.add_argument("filename", help="a comma separated value unigram file")
     parser.add_argument("-o", "--output", help="display the average word lengths over years")
     parser.add_argument("-p", "--plot", help="plot the average word lengths over years")
+    args = parser.parse_args()
+    if os.path.isfile(args.filename):
+        pass
+    else:
+        sys.stderr.write("Error: " + str(args.filename) + " does not exist!")
 
 if __name__ == '__main__':
     main()
