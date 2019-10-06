5772a441b2743a88934fb02d0dd8ee94186303ad
Ceiran <33044342+Ceiran@users.noreply.github.com>
2019-10-06 01:07:06 -0400

    Adds positional and optional arguments for word_length

--- src/word_length.py
+++ src/word_length.py
@@ -1,5 +1,23 @@
+"""
+CSAPX Project 1: Zipf's Law
+
+
+
+Author: Ayane Naito
+"""
+
+import argparse
+import sys
+import os
+from src import words_util
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("start", help="the starting year range")
+    parser.add_argument("end", help="the ending year range")
+    parser.add_argument("filename", help="a comma separated value unigram file")
+    parser.add_argument("-o", "--output", help="display the average word lengths over years")
+    parser.add_argument("-p", "--plot", help="plot the average word lengths over years")
 
 if __name__ == '__main__':
     main()
